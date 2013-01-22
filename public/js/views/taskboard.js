13a939664f3b2ff47ee195b9a022b54ab75189e8
Kelvin Pompey <kelvin.pompey@gmail.com>
2013-01-22 04:08:34 +0000

    more work on taskboard

--- public/js/views/taskboard.js
+++ public/js/views/taskboard.js
@@ -2,17 +2,27 @@ define([
 	'jquery', 
 	'underscore', 
 	'backbone',
-	'js/collections/projectcollection'], 
-	function($, _, Backbone, ProjectCollection){
-		var Taskboard = Backbone.View.extend({			
+	'js/collections/projectcollection',
+	'async'], 
+	function($, _, Backbone, ProjectCollection, async){
+		var Taskboard = Backbone.View.extend({
+			className: 'taskboard', 			
 			initialize: function() {
+				var self = this; 
 				console.log("taskboard initialize"); 
 				this.collection = new ProjectCollection;
 				this.collection.on('reset', this.collectionReset, this); 
-				this.collection.fetch(); 
+				this.collection.fetch({
+					err: function() {
+						console.log('error fetching projects'); 
+					},
+					success: function(data){
+						self.render(); 
+					}
+				});			
 
 				this.parent = this.options.parent;  
-				this.render(); 
+				
 			}, 
 
 			render: function() {
